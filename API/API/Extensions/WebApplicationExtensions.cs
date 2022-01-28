9fafe18699d5af0bee024f2396e932ae0c67f4f7
artemkarbanovich <artyomkarbanovich@gmail.com>
2022-01-28 16:17:04 +0300

    refactored Program.cs

---
+++ API/API/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static async Task ConfigureDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            var userManager = services.GetRequiredService<UserManager<AppUser>>();
+            var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+
+            await context.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occured during migration");
+        }
+    }
+}
--- API/API/Services/TokenService.cs
+++ API/API/Services/TokenService.cs
@@ -10,35 +10,30 @@ namespace API.Services;
 
 public class TokenService : ITokenService
 {
-    private readonly SymmetricSecurityKey key;
-    private readonly UserManager<AppUser> userManager;
+    private readonly SymmetricSecurityKey _key;
+    private readonly UserManager<AppUser> _userManager;
 
     public TokenService(IConfiguration config, UserManager<AppUser> userManager)
     {
-        key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
-        this.userManager = userManager;
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        _userManager = userManager;
     }
-
+    
     public async Task<string> CreateToken(AppUser appUser)
     {
-        var claims = new List<Claim>()
-        {
-            new Claim(JwtRegisteredClaimNames.NameId, appUser.Id.ToString())
-        };
-        var roles = await userManager.GetRolesAsync(appUser);
+        var claims = new List<Claim>() { new Claim(JwtRegisteredClaimNames.NameId, appUser.Id.ToString()) };
+        var roles = await _userManager.GetRolesAsync(appUser);
 
         claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
         var tokenDescriptor = new SecurityTokenDescriptor()
         {
             Subject = new ClaimsIdentity(claims),
             Expires = DateTime.Now.AddDays(7),
-            SigningCredentials = creds
+            SigningCredentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature)
         };
         var tokenHandler = new JwtSecurityTokenHandler();
-        var token = tokenHandler.CreateToken(tokenDescriptor);
 
-        return tokenHandler.WriteToken(token);
+        return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
     }
 }
