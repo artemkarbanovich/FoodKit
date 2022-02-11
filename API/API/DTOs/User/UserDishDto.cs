8423250b40263bc85a690a44e3d4b9e4417717f3
artemkarbanovich <artyomkarbanovich@gmail.com>
2022-02-11 17:03:09 +0300

    added UserDishDto attributes

--- API/API/DTOs/User/UserDishDto.cs
+++ API/API/DTOs/User/UserDishDto.cs
@@ -10,10 +10,31 @@ public class UserDishDto
     [MaxLength(30)]
     public string Name { get; set; }
 
-    [Required] public DateOnly DishDate { get; set; }
-    [Required] public int DishWeight { get; set; }
-    [Required] public float Proteins { get; set; }
-    [Required] public float Fats { get; set; }
-    [Required] public float Carbohydrates { get; set; }
-    [Required] public int Calories { get; set; }
+    [Required]
+    public DateOnly DishDate { get; set; }
+    
+    [Required]
+    [RegularExpression("^[1-9][0-9]*$")]
+    [Range(1, 5000)]
+    public int DishWeight { get; set; }
+
+    [Required]
+    [RegularExpression("^\\d*\\.?\\d*$")]
+    [Range(0, 200)]
+    public float Proteins { get; set; }
+
+    [Required]
+    [RegularExpression("^\\d*\\.?\\d*$")]
+    [Range(0, 200)]
+    public float Fats { get; set; }
+    
+    [Required]
+    [RegularExpression("^\\d*\\.?\\d*$")]
+    [Range(0, 200)]
+    public float Carbohydrates { get; set; }
+    
+    [Required]
+    [RegularExpression("^[1-9][0-9]*$")]
+    [Range(1, 1100)]
+    public int Calories { get; set; }
 }
