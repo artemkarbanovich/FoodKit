b3cca2ae9c35c3c39b1f5882b89a0f6ed26e7703
artemkarbanovich <artyomkarbanovich@gmail.com>
2022-03-15 13:59:59 +0300

    switch to https

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -31,7 +31,7 @@ app.UseCors(policy => policy
     .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
-    .WithOrigins("http://localhost:4200"));
+    .WithOrigins("https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
